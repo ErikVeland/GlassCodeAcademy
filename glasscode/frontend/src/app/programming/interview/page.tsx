bacdd24d883398f70d1b9a91871f6ec5e34c4b09
Erik Veland <erik.veland@news.com.au>
2025-10-16 23:57:41 +1000

    More Spring Cleaning

--- glasscode/frontend/src/app/programming/interview/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function InterviewRedirect() {
-  redirect('/modules/programming-fundamentals/quiz');
-}
--- glasscode/frontend/src/app/programming/interview/start/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ProgrammingInterviewStart() {
-  // Redirect to the main interview page which contains the quiz
-  redirect('/programming/interview');
-}
